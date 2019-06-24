a7ec163ff89bc02dcb911d4d076d7d7acf63b2b5
alwaysacc <alwaysacc@163.com>
2019-06-24 18:07:10 +0800

    update

--- src/views/layout/components/Sidebar/Logo.vue
+++ src/views/layout/components/Sidebar/Logo.vue
@@ -5,6 +5,7 @@
     <!--正常状态下显示的，可以使用本地的logoImg-->
     <div class="logo-con">
       <img src="http://alwaysacc.club/logo%20%282%29.png">
+<!--      <img src="https://aurora-1255840532.cos.ap-chengdu.myqcloud.com/logo.png">-->
       <!--<img :src="logoImg">-->
     </div>
   </el-menu-item>
